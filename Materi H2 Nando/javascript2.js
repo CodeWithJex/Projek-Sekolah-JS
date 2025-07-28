0d9178a51a4e1140b1f29d7ccbe7be7ebe1487ef
CodeWithJex <erlandorizki7166@gmail.com>
2025-07-28 08:05:35 +0700

    Add files via upload

---
+++ Materi H2 Nando/javascript2.js
@@ -0,0 +1,51 @@
+// Percobaan peratama Car
+// let car = {
+//     name: "Fiat",
+//     model: "500",
+//     weight: "850kg",
+//     color: "white",
+
+//     start: function(){
+//         console.log("Mobil Dinyalakan.");
+//     },
+//     drive: function(){
+//         console.log("Mobil Berjalan.");
+//     },
+//     brake: function(){
+//         console.log("Mobil Mengerem.");
+//     },
+//     stop: function(){
+//         console.log("Mobil Berhenti");
+//     }
+// };
+
+// console.log(car.name);
+// console.log(car.color);
+// car.start();
+// car.drive();
+
+// Percobaan ke dua Bicycle
+let bicycle = {
+    name: "United",
+    model: "17",
+    weight: "2kg",
+    color: "Black Doff",
+
+    start: function(){
+        console.log("Sepeda Dikayuh.");
+    },
+    drive: function(){
+        console.log("Sepeda Mengerem.");
+    },
+    brake: function(){
+        console.log("Sepeda Mengerem.");
+    },
+    stop: function(){
+        console.log("Sepeda Berhenti");
+    }
+};
+
+console.log(bicycle.name);
+console.log(bicycle.color);
+bicycle.start();
+bicycle.drive();
